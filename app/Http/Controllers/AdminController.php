4b6de9c9ae7d73d86d1a827f4eeae0246e527e93
kevin-1111102 <97698002+Kevin-1111102@users.noreply.github.com>
2026-03-30 16:59:50 +0200

    fix

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,7 +15,7 @@ class AdminController extends Controller
     public function index(): View
     {
         $users = User::all();
-        return view('admin.users.index', compact('users'));
+        return view('admin.users', compact('users'));
     }
 
     /**
